2a46dd786c98c4451007d609bd87f10fd0d096e9
takayuki-kono <xx03dragon@gmail.com>
2025-02-11 02:48:28 +0900

    ランドマーク組み込み前仮プッシュ

--- train_and_convert_model.py
+++ train_and_convert_model.py
@@ -1,26 +1,45 @@
 import os
+import shutil
 from timeit import repeat
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 import math
+import pathlib
 from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-try:
-    tf.get_logger().setLevel(tf.get_log('ERROR'))  # 'ERROR'で情報メッセージを非表示に
-except Exception as e:
-    print(f"Error processing training : {e}")
+
+# try:
+#     tf.get_logger().setLevel(tf.get_log('ERROR'))  # 'ERROR'で情報メッセージを非表示に
+# except Exception as e:
+#     print(f"Error processing training : {e}")
 
 # ディレクトリのパス
 TRAIN_DIR = 'train'
 VALIDATION_DIR = 'validation'
 PREPROCESSED_TRAIN_DIR = 'preprocessed/train'
-PREPROCESSED_VALIDATION_DIR = 'preprocessed/validation'
+PREPROCESSED_VALIDATION_DIR = 'preprocessed/validation' 
+# 要調整
+# 20 0.68 0.52
+# 40 0.7  0.5
+# 10 0.72 0.55
+#  0 0.72 0.47
+# 15 0.72 0.44
+ROTATION_RANGE = 10
+# 要調整
+# 2 0.69 0.63
+BATCH_SIZE = 2
 
 # 要調整
+# 56  0.63 0.48
+# 84  0.67 0.43
+# 112 0.68 0.53
+# 140 0.68 0.51
+# 168 0.71 0.47
+# 224 0.68 0.41
 img_size = 112
 
 # MediaPipeの顔検出モデルの初期化
@@ -29,6 +48,9 @@ face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
 
 # 顔の切り抜きとリサイズの処理
 def preprocess_and_cut_faces(input_dir, output_dir):
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -59,11 +81,18 @@ def preprocess_and_cut_faces(input_dir, output_dir):
 
                             # 顔の部分を切り抜いてリサイズ
                             face_image = img[y:y + height, x:x + width]
-                            face_image_resized = cv2.resize(face_image, (img_size, img_size))
 
-                            # 保存
-                            output_path = os.path.join(category_output_dir, filename)
-                            cv2.imwrite(output_path, face_image_resized)
+                            # face_image_resized = cv2.resize(face_image, (img_size, img_size))
+
+                            # # 保存
+                            # output_path = os.path.join(category_output_dir, filename)
+                            # cv2.imwrite(output_path, face_image_resized)
+                            if face_image is not None and face_image.size > 0:
+                                face_image_resized = cv2.resize(face_image, (img_size, img_size))
+                                output_path = os.path.join(category_output_dir, filename)
+                                cv2.imwrite(output_path, face_image_resized)
+                            else:
+                                print(f"Skipping {filename} due to empty face image.")
                     else:
                         print(f"No face detected in {filename}, skipping.")
             except Exception as e:
@@ -79,25 +108,28 @@ def create_model():
     from tensorflow.keras.applications import VGG16, ResNet50, MobileNet
 
     # 要調整
-    # # VGG16 0.55
-    # base_model = tf.keras.applications.VGG16(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
+    # # VGG16 0.68 0.63
+    base_model = tf.keras.applications.VGG16(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
-    # # ResNet50 0.37
+    # # ResNet50 0.54 0.44
     # base_model = tf.keras.applications.ResNet50(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
-    # InceptionV3 299 0.52
+    # InceptionV3 299 0.65 0.5
     # base_model = tf.keras.applications.InceptionV3(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
-    # # MobileNetV2 0.42
+    # # MobileNetV2 0.73 0.5
     # base_model = tf.keras.applications.MobileNetV2(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
-    # # DenseNet121 0.63
-    base_model = tf.keras.applications.DenseNet121(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
+    # # DenseNet121 0.67 0.55
+    # base_model = tf.keras.applications.DenseNet121(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
-    # # EfficientNetB0 0.45
+    # # EfficientNetB0 0.49 0.47
     # base_model = tf.keras.applications.EfficientNetB0(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
 
     base_model.trainable = False  # 転移学習のため、ベースモデルは固定
+    # base_model.trainable = True
+    # for layer in base_model.layers[:-20]:  # 最後の20層以外を凍結
+    #     layer.trainable = False
 
     model = models.Sequential([
         base_model,
@@ -144,35 +176,44 @@ def create_model():
     #                     validation_data=validation_generator,
     #                     epochs=30,
     #                     callbacks=[lr_scheduler])
+
     return model
 
 # データジェネレーターの作成
 train_datagen = ImageDataGenerator(
     rescale=1./255,
-    # 要調整
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
+    # 0.95 1.05
+    # 0.7  1.3  0.77 0.65
+    # 0.95  1.05  0.77 0.65
+    brightness_range=[1, 1],  # 明るさを 0.7～1.3 倍に変化
+    # contrast_stretching=True,  # コントラストを変化させる
+    rotation_range=ROTATION_RANGE,  # 回転
+    width_shift_range=0.2,  # 横方向のずれ
+    height_shift_range=0.2,  # 縦方向のずれ
+    shear_range=0.2,  # 斜め方向の変形
+    zoom_range=0.2,  # 拡大・縮小
+    horizontal_flip=True  # 左右反転
 )
-validation_datagen = ImageDataGenerator(
-    rescale=1./255,
-    # 要調整
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-)
+
+# validation_datagen = ImageDataGenerator(
+#     rescale=1./255,
+#     brightness_range=[0.7, 1.3],  # 明るさを 0.7～1.3 倍に変化
+#     # contrast_stretching=True,  # コントラストを変化させる
+#     rotation_range=ROTATION_RANGE,  # 回転
+#     width_shift_range=0.2,  # 横方向のずれ
+#     height_shift_range=0.2,  # 縦方向のずれ
+#     shear_range=0.2,  # 斜め方向の変形
+#     zoom_range=0.2,  # 拡大・縮小
+#     horizontal_flip=True  # 左右反転
+# )
+
+validation_datagen = ImageDataGenerator(rescale=1./255)  # 検証データは変更しない
 
 train_generator = train_datagen.flow_from_directory(
     PREPROCESSED_TRAIN_DIR,
     target_size=(img_size, img_size),
     # 要調整
-    batch_size=2,
+    batch_size=BATCH_SIZE,
     class_mode='sparse'
 )
 
@@ -180,7 +221,7 @@ validation_generator = validation_datagen.flow_from_directory(
     PREPROCESSED_VALIDATION_DIR,
     target_size=(img_size, img_size),
     # 要調整
-    batch_size=2,
+    batch_size=BATCH_SIZE,
     class_mode='sparse'
 )
 
@@ -189,7 +230,7 @@ try:
     model = create_model()
 
     model_checkpoint = ModelCheckpoint('best_model.keras', monitor='val_accuracy', save_best_only=True)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=6)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=12)
 
     # # `repeat()`を使って無限ループを設定
     # train_dataset = tf.data.Dataset.from_generator(
